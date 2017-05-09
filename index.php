358bdb51f55ed7a4fb9c0b175efaa71d7c15628b
Velliz <diditvelliz@gmail.com>
2017-05-09 13:53:25 +0700

    url fix

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 define('ROOT', __DIR__);
-define('BASE_URL', $_SERVER['REQUEST_SCHEME'] . "://" . $_SERVER['SERVER_NAME'] . '/');
+define('BASE_URL', $_SERVER['REQUEST_SCHEME'] . "://" . $_SERVER['SERVER_NAME'] . '/pukoexample/');
 
 require __DIR__ . '/vendor/autoload.php';
 
