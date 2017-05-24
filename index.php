d1ae95b18e5a50a023cc0413c496f75229fdca32
velliz <diditvelliz@outlook.com>
2017-05-24 21:11:25 +0700

    add start microtime

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ define('ROOT', __DIR__);
 define('BASE_URL', $_SERVER['REQUEST_SCHEME'] . "://" . $_SERVER['SERVER_NAME'] . '/pukoexample/');
 
 require __DIR__ . '/vendor/autoload.php';
+define('START', microtime(true));
 
 $framework = new \pukoframework\Framework();
 $framework->Start();
